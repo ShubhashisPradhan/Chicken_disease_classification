5ae4f05cb02e657427b0eb6dcafd89e675fd0fe9
Shubhashis Pradhan <iamshubhashis@gmail.com>
2024-05-13 11:14:16 +0530

    prepare base model done

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from src.cnnClclassifire import logger
 from src.cnnClclassifire.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.cnnClclassifire.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+
+
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -10,3 +13,14 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+STAGE_NAME = "Prepare Base Model"
+try:
+      logger.info(f"***************************************")
+      logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+      prepare_base_model = PrepareBaseModelTrainingPipeline()
+      prepare_base_model.main()
+      logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+      logger.exception(e)
+      raise e
