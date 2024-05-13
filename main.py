580cd834930672288b973a6548f0f32a9094be19
Shubhashis Pradhan <iamshubhashis@gmail.com>
2024-05-13 17:29:08 +0530

    training added

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from cnnClclassifire.pipeline.stage_03_training import ModelTrainingPipeline
 from src.cnnClclassifire import logger
 from src.cnnClclassifire.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.cnnClclassifire.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
@@ -16,11 +17,23 @@ except Exception as e:
 
 STAGE_NAME = "Prepare Base Model"
 try:
-      logger.info(f"***************************************")
-      logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-      prepare_base_model = PrepareBaseModelTrainingPipeline()
-      prepare_base_model.main()
-      logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+   logger.info(f"***************************************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-      logger.exception(e)
-      raise e
+   logger.exception(e)
+   raise e
+
+STAGE_NAME = "Training"
+
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   obj = ModelTrainingPipeline()
+   obj.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+   logger.exception(e)
+   raise e
